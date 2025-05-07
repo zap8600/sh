c7c49eb4ef10302d8c8281f2e10aa986c9b90e8f
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-05-07 19:48:18 +0000

    wip

--- sh.c
+++ sh.c
@@ -3,18 +3,31 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main() {
     char* input = NULL;
+    char** command = NULL;
     long long int ccount = 0;
+    long long int cmcount = 0;
     int ch = EOF;
-    while((ch = fgetc(stdin)) != '\n') {
-        input = (char*)realloc(input, ++ccount);
-        input[ccount - 1] = (char)ch;
+    while(1) {
+        while((ch = fgetc(stdin)) != '\n') {
+            input = (char*)realloc(input, ++ccount);
+            input[ccount - 1] = (char)ch;
+        }
+        input = (char*)realloc(input, ccount + 1);
+        input[ccount] = '\0';
+        char* cch = input;
+        char* ccmd = input;
+        while(1) {
+            if(*cch == ' ' || *cch == '\0') {
+                *cch++ = '\0';
+                command = (char**)realloc(command, ++cmcount * sizeof(char*));
+                command[cmcount - 1] = ccmd;
+                ccmd = cch;
+            }
+        }
     }
-    input = (char*)realloc(input, ccount + 1);
-    input[ccount] = '\0';
-    puts(input);
-    free(input);
     return 0;
 }
